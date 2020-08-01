e805b5ee95db18aed7a17b0d9c6bce47590b0b6e
yuriy.shpak <yuriy.shpak@itmint.ca>
2020-08-02 00:46:37 +0300

    remove unused using, remove comments

--- BusinessLayer/HomesManager.cs
+++ BusinessLayer/HomesManager.cs
@@ -30,58 +30,5 @@ namespace BusinessLayer
         {
             return _mapper.Map<IList<HomeModel>>(_animalsRepo.GetAllHomes());
         }
-
-        /*public BookDTO GetBookById(int id)
-        {
-            return _mapper.Map<BookDTO>(_bookStoreRepo.GetBookById(id));
-        }
-
-        public void AddBook(BookDTO bookDTO)
-        {
-            bookDTO.WriterIds.ForEach(bookWriterId =>
-            {
-                bookDTO.Writers.Add( new WriterDTO {Id= bookWriterId});
-            });
-            bookDTO.Category.Id = bookDTO.CategoryId;
-
-            _bookStoreRepo.SaveBook(_mapper.Map<Book>(bookDTO));
-        }
-
-        public void UpdateBook(BookDTO bookDTO)
-        {
-            bookDTO.Category.Id = bookDTO.CategoryId;
-
-            bookDTO.WriterIds.ForEach(bookWriterId =>
-            {
-                bookDTO.Writers.Add(new WriterDTO { Id = bookWriterId });
-            });
-
-            _bookStoreRepo.UpdateBook(_mapper.Map<Book>(bookDTO));
-        }
-
-        public void RemoveBookById(int id)
-        {
-            _bookStoreRepo.RemoveBookById(id);
-        }
-
-        public IList<CategoryDTO> GetAllCategories()
-        {
-            return _mapper.Map<IList<CategoryDTO>>(_bookStoreRepo.GetAllCategories());
-        }
-
-        public CategoryDTO GetCategoryById(int id)
-        {
-            return _mapper.Map<CategoryDTO>(_bookStoreRepo.GetCategoryById(id));
-        }
-
-        public IList<WriterDTO> GetAllWriters()
-        {
-            return _mapper.Map<IList<WriterDTO>>(_bookStoreRepo.GetAllWriters());
-        }
-
-        public WriterDTO GetWriterById(int id)
-        {
-            return _mapper.Map<WriterDTO>(_bookStoreRepo.GetWriterById(id));
-        }*/
     }
 }
